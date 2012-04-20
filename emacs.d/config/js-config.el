f03f521ca32bff53cf20f684504f16660d56a4b5
pokutuna <popopopopokutuna@gmail.com>
2012-04-20 21:42:15 +0900

    move js2-mode to submodule

--- emacs.d/config/js-config.el
+++ emacs.d/config/js-config.el
@@ -1,3 +1,5 @@
+(add-to-load-path "co/js2-mode")
+
 ;; indent 4 spaces
 (setq-default c-basic-offset 4)
 
@@ -6,15 +8,17 @@
 ;; http://d.hatena.ne.jp/sabottenda/20110526/1306411032
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
 (add-hook 'js2-mode-hook
           '(lambda ()
              (define-key js2-mode-map "\M-n" 'next-error)
              ))
 
 
-(defun js-mode-hooks ()
-  (setq flymake-jsl-mode-map 'js-mode-map)
-  (when (require 'flymake-jsl nil t)
-    (setq flymake-check-was-interrupted t)
-    (flymake-mode t)))
-(add-hook 'js-mode-hook 'js-mode-hooks)
+
+;; (defun js-mode-hooks ()
+;;   (setq flymake-jsl-mode-map 'js-mode-map)
+;;   (when (require 'flymake-jsl nil t)
+;;     (setq flymake-check-was-interrupted t)
+;;     (flymake-mode t)))
+;; (add-hook 'js-mode-hook 'js-mode-hooks)
