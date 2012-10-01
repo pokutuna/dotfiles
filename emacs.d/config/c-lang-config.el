b524a6869d95077f348857d7f0ec9ea34b010b6d
pokutuna <popopopopokutuna@gmail.com>
2012-10-01 15:20:42 +0900

    c-lang

--- emacs.d/config/c-lang-config.el
+++ emacs.d/config/c-lang-config.el
@@ -1,4 +1,10 @@
+;; general
 (add-hook 'c-mode-common-hook
           '(lambda ()
              (c-toggle-auto-hungry-state 1)
-             (define-key c-mode-base-map "\C-m" 'newline-and-indent)))
+             (define-key c-mode-base-map "\C-m" 'newline-and-indent)
+             (c-set-style "k&r")
+             (setq indent-tabs-mode nil)
+             (setq c-basic-offset 2)
+             (parenthesis-register-keys "{('\"[" c-mode-map)
+             ))
