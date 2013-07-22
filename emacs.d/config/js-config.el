ac8e6c5fa1a0cd17e928e9fb69d09e8776973e08
pokutuna <popopopopokutuna@gmail.com>
2013-07-22 14:36:15 +0900

    flymake-json

--- emacs.d/config/js-config.el
+++ emacs.d/config/js-config.el
@@ -5,10 +5,17 @@
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
 (lazyload (js2-mode) "js2-mode"
-          (setq-default js2-basic-offset 2))
+          ;; (setq-default js2-basic-offset 2)
+          (setq-default js2-basic-offset 4)
+          )
 
 (add-hook-fn 'js2-mode-hook
-             (define-key js2-mode-map "\M-n" 'next-error))
+             (define-key js2-mode-map "\M-n" 'next-error)
+
+             ;; requires jsonlint, `$ npm install jsonlint -g`
+             ;; TODO こういうの一発でいれたい
+             (flymake-json-maybe-load)
+             )
 
 ;; (defun js-mode-hooks ()
 ;;   (setq flymake-jsl-mode-map 'js-mode-map)
