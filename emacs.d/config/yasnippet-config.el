fcb4f11dd40f2f0af1b4279ce4320ef44437978f
pokutuna <popopopopokutuna@gmail.com>
2013-08-26 02:39:28 +0900

    udpate yas & edit configs

--- emacs.d/config/yasnippet-config.el
+++ emacs.d/config/yasnippet-config.el
@@ -7,6 +7,7 @@
 (yas-reload-all)
 (yas-global-mode 1)
 
+(global-set-key (kbd "C-c C-s") 'yas-insert-snippet)
 
 ;; dropdown-list.el from comment in yasnippet.el
 (require 'dropdown-list)
@@ -38,3 +39,22 @@
              (when flymake-is-active-flag
                (flymake-mode-on)
                (setq flymake-is-active-flag nil))))
+
+
+;; anything interface
+(eval-after-load "anything-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (anything-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*anything yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))))
