54d067f1b74db25cb6e82ff1dee227241ca6e418
pokutuna <popopopopokutuna@gmail.com>
2013-08-26 03:52:29 +0900

    update some packages

---
+++ emacs.d/elpa/cperl-mode-20130619.1241/cperl-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cperl-mode" "20130619.1241" "Perl code editing commands for Emacs [github]" (quote nil))
---
+++ emacs.d/elpa/melpa-20130815.2147/melpa-pkg.el
@@ -0,0 +1,1 @@
+(define-package "melpa" "20130815.2147" "special handling for the MELPA repository [github]" (quote nil))
