ca7de23ae53e6aa2655919ad52f138e8504b3d59
pokutuna <popopopopokutuna@gmail.com>
2014-01-01 02:10:45 +0900

    install init-loader.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,40 @@
+;; ~/.emacs.dから相対的にload-path追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+(add-to-load-path "elisp")
+
+
+;; PATH
+(setq shell-file-name "/usr/local/bin/zsh") ;;zshenv見に行く
+(add-to-list 'exec-path "/usr/local/bin")
+(setenv "PATH" (mapconcat 'identity exec-path ":"))
+
+
+;; LANG
+(setenv "LANG" "ja_JP.UTF-8")
+
+
+;; package.el
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
+;; init-loader
+(require 'init-loader)
+(init-loader-load "~/.emacs.d/inits")
+
+
+
 (load-file "~/.emacs.d/config/init-helper-macro.el")
-(load-file "~/.emacs.d/config/load-path-config.el") ;; load-path & PATH
 (load-file "~/.emacs.d/config/basic-config.el")
 (load-file "~/.emacs.d/config/darwin-config.el")
 (load-file "~/.emacs.d/config/face-config.el")
