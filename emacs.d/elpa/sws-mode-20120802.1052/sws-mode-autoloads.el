419289325f8d3714431351e36ac95a4d3de54716
pokutuna <popopopopokutuna@gmail.com>
2013-12-31 23:08:38 +0900

    update elpa

---
+++ emacs.d/elpa/sws-mode-20120802.1052/sws-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; sws-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (sws-mode) "sws-mode" "sws-mode.el" (21168 13554))
+;;; Generated autoloads from sws-mode.el
+
+(autoload 'sws-mode "sws-mode" "\
+Major mode for editing significant whitespace files
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("sws-mode-pkg.el") (21168 13554 401931))
+
+;;;***
+
+(provide 'sws-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sws-mode-autoloads.el ends here
