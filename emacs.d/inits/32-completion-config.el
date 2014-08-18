312143742373339cf41fc36afefbf4738c2ac03a
pokutuna <popopopopokutuna@gmail.com>
2014-08-18 19:51:56 +0900

    smartchr

--- emacs.d/inits/32-completion-config.el
+++ emacs.d/inits/32-completion-config.el
@@ -1,7 +1,7 @@
 ;;smartchr
 ;;(install-elisp "http://github.com/imakado/emacs-smartchr/raw/master/smartchr.el")
 (when (require 'smartchr nil t)
-  (define-key global-map (kbd "=") (smartchr '("=" "==" "===")))
+  ; (define-key global-map (kbd "=") (smartchr '("=" "==" "===")))
   (define-key global-map (kbd "+") (smartchr '("+" "++")))
   (define-key global-map (kbd "{") (smartchr '("{ `!!' }" "{")))
   (define-key global-map (kbd ">")
--- emacs.d/inits/55-go-lang-config.el
+++ emacs.d/inits/55-go-lang-config.el
@@ -1,8 +1,11 @@
 (require 'go-mode)
 (eval-after-load "go-mode"
   '(progn
-
      (require 'go-autocomplete)
      (add-hook 'go-mode-hook 'go-eldoc-setup)
      (add-hook 'before-save-hook 'gofmt-before-save)
+
+     (when (require 'smartchr nil t)
+       (define-key go-mode-map (kbd "=") (smartchr '("=" ":=")))
+       )
      ))
