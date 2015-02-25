13e6e3e5dfe9e18d449ca71bedaf1165c7730d6b
pokutuna <popopopopokutuna@gmail.com>
2015-02-25 11:45:40 +0900

    update elpa

--- emacs.d/elpa/paredit-20150213.1427/paredit-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "paredit" "20150213.1427" "minor mode for editing parentheses" (quote nil))
--- emacs.d/inits/10-util-config.el
+++ emacs.d/inits/10-util-config.el
@@ -154,3 +154,30 @@
 ;; git-gutter-fringe
 (require 'git-gutter-fringe)
 (global-git-gutter-mode t)
+
+
+;; copy-as-rtf, copy-as-html
+;; http://micubiculo.com/copy-as-rtf-in-emacs/
+;; requires `highlight` command
+;; (setq copy-as-rtf-theme "zenburn")
+
+;; (defun copy-as-rtf (start end)
+;;   (interactive "r")
+;;   (copy-as "rtf" start end ""))
+
+;; (defun copy-as-html (start end)
+;;   (interactive "r")
+;;   (copy-as "html" start end "--include-style"))
+
+;; (defun copy-as (format start end extra-options)
+;;   (let* (
+;;     (temp-file (make-temp-file "copy-as-rtf"))
+;;     (file-format (file-name-extension (buffer-file-name)))
+;;     (selected-text (buffer-substring start end))
+;;     (common-options (concat "--out-format=" format " --src-lang " file-format " --style " copy-as-rtf-theme " "))
+;;     (command (concat "highlight " common-options extra-options " -i " temp-file " | pbcopy "))
+;;     )
+;;     (append-to-file start end temp-file)
+;;     (shell-command command)
+;;     (deactivate-mark)
+;;     (message "RTF copied to clipboard")))
