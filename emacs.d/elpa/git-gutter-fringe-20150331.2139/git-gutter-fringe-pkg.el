f59ce97caf90f6e0e3c6aa84c01d4c1affb9a66c
pokutuna <popopopopokutuna@gmail.com>
2015-04-08 10:41:34 +0900

    update elpa

---
+++ emacs.d/elpa/git-gutter-fringe-20150331.2139/git-gutter-fringe-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-gutter-fringe" "20150331.2139" "Fringe version of git-gutter.el" (quote ((git-gutter "0.82") (fringe-helper "0.1.1") (cl-lib "0.5") (emacs "24"))))
--- emacs.d/elpa/inf-ruby-20150222.1742/inf-ruby-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "inf-ruby" "20150222.1742" "Run a Ruby process in a buffer" (quote nil))
