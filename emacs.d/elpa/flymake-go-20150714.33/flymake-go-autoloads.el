0557f32c6000a4135a69c6377c9ef923b18621e4
pokutuna <popopopopokutuna@gmail.com>
2015-07-17 20:26:33 +0900

    update elpa

---
+++ emacs.d/elpa/flymake-go-20150714.33/flymake-go-autoloads.el
@@ -0,0 +1,15 @@
+;;; flymake-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flymake-go.el") (21928 57848 335887 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymake-go-autoloads.el ends here
