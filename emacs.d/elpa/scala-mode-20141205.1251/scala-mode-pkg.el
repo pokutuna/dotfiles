ea4c6ff28b95495cfcef56d185ff2561e5ea0e65
pokutuna <popopopopokutuna@gmail.com>
2016-04-07 11:58:16 +0900

    scala

--- emacs.d/elpa/scala-mode-20141205.1251/scala-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "scala-mode" "20141205.1251" "Major mode for editing Scala code." 'nil :keywords
-  '("scala" "languages" "oop"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
