98320aea1e3226927c988c72ca1bb9dc486f51b6
pokutuna <popopopopokutuna@gmail.com>
2020-07-08 08:06:19 +0900

    localssh.sh

---
+++ bin/localssl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CWD=$(cd $(dirname "$0") && pwd)
+
+if [ $# == 0 ]; then
+  echo 'localssl.sh {on|off} {port}'
+  exit
+fi
+
+if [ $1 == 'on' ]; then
+  set -x
+  docker-compose -f $CWD/localssl/docker-compose.yaml up -d
+elif [ $1 == 'off' ]; then
+  set -x
+  docker-compose -f $CWD/localssl/docker-compose.yaml down
+fi
