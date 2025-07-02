57c3f3c22eff95f7c48c81ab3bc1644e9ffe900d
pokutuna <mail@pokutuna.com>
2025-07-02 15:26:32 +0900

    mytemp

--- zsh.d/tmp.sh
+++ zsh.d/tmp.sh
@@ -1,13 +1,13 @@
-tmp(){
+mytemp(){
   read YEAR MONTH DAY < <(date '+%Y %m %d')
-  TMPDIR="$HOME/tmp/$YEAR/$MONTH"
+  TMPDIR="$HOME/mytmp/$YEAR/$MONTH"
   mkdir -p $TMPDIR
   cd `mktemp -d --tmpdir="$TMPDIR" $DAY.$1${1:+.}XXXXXX`
 }
 
-tmpd() {
+mytempd() {
   read YEAR MONTH DAY < <(date '+%Y %m %d')
-  TMPDIR="$HOME/tmp/$YEAR/$MONTH"
+  TMPDIR="$HOME/mytmp/$YEAR/$MONTH"
   mkdir -p $TMPDIR
   cd $TMPDIR
 }
