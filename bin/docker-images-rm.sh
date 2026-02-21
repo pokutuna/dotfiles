33491f84d1f6b5b29efdeef67a50fb6252c14fee
pokutuna <mail@pokutuna.com>
2026-02-21 20:48:12 +0900

    dockerimages

--- bin/docker-images-rm.sh
+++ bin/docker-images-rm.sh
@@ -1,1 +1,4 @@
-docker images | fzf --exit-0 --multi --header-lines=1 | awk '{ print $3 }' | xargs docker image rm -f
+docker images --format '{{.Repository}}:{{.Tag}}\t{{.Size}}\t{{.ID}}' \
+  | fzf --exit-0 --multi --header='TABで複数選択, Enterで確定' \
+  | awk '{print $NF}' \
+  | xargs docker rmi
